7c90810d4ba7a267129fa552a44ec8f00c50ca3c
Andreas Schürmann <andreas@adhook.io>
2019-09-03 13:35:04 +0200

    Initial Commit

--- ts-jest/app.test.ts
+++ ts-jest/app.test.ts
@@ -1,9 +1,3 @@
-import { sum } from '../src/app';
-
 test('basic', () => {
-  //expect(sum()).toBe(0);
-});
-
-test('basic again', () => {
-  //expect(sum(1, 2)).toBe(3);
+  expect(true).toBe(true);
 });
