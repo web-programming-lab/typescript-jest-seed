c03fa2bbd0edbd19fb57dc906a99db1d3a03cd3d
Andreas Schürmann <andreas@adhook.io>
2022-02-08 11:09:52 +0100

    Cleanup

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ interface KeyPair {
   key: string;
   value: string;
 }
-let kv1: KeyPair = { key:"key", value:"value" };
+let kv1: KeyPair = { key:"key", value:"value" };
 let kv2: KeyPair = { key:1, value:"Steve" };
 let kv3: KeyPair = { key:1, value:12345 };
